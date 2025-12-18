c77d828b5d6581bc83925959057099780eb67b9a
Diego Ferreyra <diegofacundoferreyra@gmail.com>
2025-12-17 21:14:52 -0300

    De a poco pero va

---
+++ src/components/CartContainer.jsx
@@ -0,0 +1,16 @@
+//👉 Es un componente contenedor (Container Component)
+//No muestra diseño propio, decide qué componente mostrar según el estado del carrito.
+
+//📌 Qué hace exactamente este archivo
+//const { cart } = useContext(CartContext);
+//🔹 Obtiene el estado global del carrito desde el CartContext.
+
+//return <>{!cart.length ? <EmptyCart /> : <CartView />}</>;
+
+//🔹 Acá está la lógica principal:
+//Estado del carrito	Qué se muestra
+//cart.length === 0	<EmptyCart />
+//cart.length > 0	<CartView />
+
+// El usuario nunca ve un carrito vacío mal presentado
+//React decide automáticamente qué renderizar
